d2adc6a3b1d2cc0a6521cdc693220579a03be91f
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-04 16:57:07 +0530

    added more crud to exercise

--- routes/exercises.js
+++ routes/exercises.js
@@ -39,4 +39,20 @@ router.route("/:id").delete((req, res) => {
     .catch((err) => res.status(400), json("Error:" + err));
 });
 
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise has been updated"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
