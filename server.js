cfe0798070f5d5b68218b8b414e05a585f247c09
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-04 13:35:33 +0530

    error in routing

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
 app.use(cors());
 app.use(express.json());
 
