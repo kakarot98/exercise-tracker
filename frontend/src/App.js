03a93cba673ec183029d7b94cf032952ddb64a0a
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-05 17:11:40 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar.component'
-import ExerciseList from './components/ExerciseList.component'
-import EditExercise from './components/EditExercise.component'
-import CreateExercise from './components/CreateExercise.component'
-import CreateUser from './components/CreateUser.component'
+import NavBar from "./components/NavBar.component";
+import ExerciseList from "./components/ExerciseList.component";
+import EditExercise from "./components/EditExercise.component";
+import CreateExercise from "./components/CreateExercise.component";
+import CreateUser from "./components/CreateUser.component";
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <br/>
-    <Route path="/" exact component={ExerciseList} />
-    <Route path="/edit/:id" exact component={EditExercise} />
-    <Route path="/create" exact component={CreateExercise} />
-    <Route path="/user" exact component={CreateUser} />
+      <div className="container">
+        <NavBar />
+        <br />
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" exact component={EditExercise} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- frontend/src/components/CreateExercise.component.js
+++ frontend/src/components/CreateExercise.component.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const CreateExercise = () => {
-    return (
-        <div>
-            Create Exercises here
-        </div>
-    )
-}
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
 
-export default CreateExercise
+  useEffect(() => {
+    setUsername("test user");
+    setUsers(["test user"]);
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercises = {
+      username: username,
+      description: description,
+      duration: duration,
+      date: date,
+      users: users,
+    };
+    console.log(exercises);
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <h3>Create Exercise Logs here</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            
+            required
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          >
+            {users.map((user) => {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateExercise;
