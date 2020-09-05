9f4e34e223e659f84959d1ea9a5c4b98dc112e9c
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-05 14:00:45 +0530

    creating frontend

--- frontend/src/components/NavBar.component.js
+++ frontend/src/components/NavBar.component.js
@@ -2,7 +2,22 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const NavBar = () => {
-  return <nav className="navbar navbar-dark bg-dark navbar-expand-lg"></nav>;
+  return (<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">ExerciseTracker</Link>
+      <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                  <Link to="/" className="nav-link">Exercises</Link>
+              </li>
+              <li className="navbar-item">
+                  <Link to="/" className="nav-link">Create exercise Log</Link>
+              </li>
+              <li className="navbar-item">
+                  <Link to="/" className="nav-link">Create user</Link>
+              </li>
+          </ul>
+      </div>
+  </nav>)
 };
 
 export default NavBar;
