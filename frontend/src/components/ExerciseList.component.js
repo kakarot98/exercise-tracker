d84797598b3a4551373404357c2358d9044bb763
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-06 11:11:31 +0530

    trying out delete exercise requests

--- frontend/src/components/ExerciseList.component.js
+++ frontend/src/components/ExerciseList.component.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 const ExerciseList = () => {
-    return (
-        <div>
-            Exercise list here
-        </div>
-    )
-}
+  const [exercisesList, setExercisesList] = useState([]);
 
-export default ExerciseList
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then((response) => setExercisesList(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const deletExercise = (id) => {
+    axios
+      .delete("http://localhost:5000/exercises/" + id)
+      .then((response) => {
+        console.log(response.data);
+        setExercisesList(exercisesList.filter((exer) => exer.id !== id));
+        console.log("exercise has been deleted from the list as well!");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return <div>Exercise list here</div>;
+};
+
+export default ExerciseList;
